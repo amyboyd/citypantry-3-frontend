1938bf7e4d72b3719c9990639deabee5ae772cc0
Amy Boyd <amy@amyboyd.co.uk>
2015-08-14 16:43:54 +0100

    Make font size of invoices readable in the staff pages.

--- src/web/js/directives/cpCustomerInvoiceDirective.js
+++ src/web/js/directives/cpCustomerInvoiceDirective.js
@@ -4,7 +4,17 @@ angular.module('cp').directive('cpCustomerInvoice', function(OrdersFactory) {
         link: function(scope, element, attrs) {
             OrdersFactory.getCustomerInvoiceAsHtml(attrs.cpInvoiceId)
                 .then(response => {
-                    element.html(response.data);
+                    let invoiceHtml = response.data;
+
+                    // Change the invoice's 'body' CSS to only apply to this directive, not the rest
+                    // of the page. This is so the rest of the page doesn't not get changed.
+                    invoiceHtml = invoiceHtml.replace(/<style>\s+body \{/, '<style>cp-customer-invoice {');
+
+                    // Make the invoice text readable. The font-size rule is correct for the PDF
+                    // renderer but not for this page.
+                    invoiceHtml = invoiceHtml.replace('font-size: 62.5%;', '');
+
+                    element.html(invoiceHtml);
                 });
         }
     };
