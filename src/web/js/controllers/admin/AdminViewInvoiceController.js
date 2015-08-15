7833046b940cf432fc4893864e1050d620138ded
Amy Boyd <amy@amyboyd.co.uk>
2015-08-15 14:50:45 +0100

    Add links to mark invoice as paid/unpaid on the staff invoice page.

--- src/web/js/controllers/admin/AdminViewInvoiceController.js
+++ src/web/js/controllers/admin/AdminViewInvoiceController.js
@@ -1,6 +1,6 @@
 angular.module('cp.controllers.admin').controller('AdminViewInvoiceController',
         function($scope, $routeParams, OrdersFactory, NotificationService, DocumentTitleService,
-        SecurityService, LoadingService) {
+        SecurityService, LoadingService, INVOICE_STATUS_AWAITING_PAYMENT, INVOICE_STATUS_PAID) {
     DocumentTitleService('View invoice');
     SecurityService.requireStaff();
 
@@ -10,4 +10,23 @@ angular.module('cp.controllers.admin').controller('AdminViewInvoiceController',
             LoadingService.hide();
         })
         .error(response => NotificationService.notifyError(response.errorTranslation));
+
+    function updateStatus(status) {
+        LoadingService.show();
+
+        OrdersFactory.updateCustomerInvoiceStatus($scope.invoice.id, status)
+            .success(response => {
+                $scope.invoice = response.invoice;
+                LoadingService.hide();
+            })
+            .error(response => NotificationService.notifyError(response.errorTranslation));
+    }
+
+    $scope.markAsPaid = function() {
+        updateStatus(INVOICE_STATUS_PAID);
+    };
+
+    $scope.markAsAwaitingPayment = function() {
+        updateStatus(INVOICE_STATUS_AWAITING_PAYMENT);
+    };
 });
